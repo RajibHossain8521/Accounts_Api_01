ca2f3cbde59a4fc8230b1086ad2757a535137f67
rajibhossain8521 <rajibhossain8521@gmail.com>
2021-01-22 21:30:44 +0600

    Defined all View for Projects

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+class UserRegistrationView():
+    pass
+
+
+class VerifyEmailView():
+    pass
+
+
+class ResendVerifyEmailView():
+    pass
+
+
+class LoginView():
+    pass
+
+
+class LogoutView():
+    pass
+
+
+class ResetPasswordEmailView():
+    pass
+
+
+class ResetPasswordTokenCheckView():
+    pass
+
+
+class SetNewPasswordView():
+    pass
