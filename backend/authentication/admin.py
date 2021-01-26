476ccc3c19c72a82f6452db7cca5cb35b2617ff9
rajibhossain8521 <rajibhossain8521@gmail.com>
2021-01-26 22:43:58 +0600

    Registered UserAdmin and UserAccounts tables

--- backend/authentication/admin.py
+++ backend/authentication/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import UserAccounts
 
 # Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'created_at']
+
+admin.site.register(UserAccounts, UserAdmin)
